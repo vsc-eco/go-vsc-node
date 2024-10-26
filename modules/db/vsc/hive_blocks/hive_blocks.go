21f2543f22b607f331a0930ebcc70deb794a221a
Matthew Trent <matthew.rl.trent@gmail.com>
2024-10-26 04:10:25 -0700

    wip: stream tests

--- modules/db/vsc/hive_blocks/hive_blocks.go
+++ modules/db/vsc/hive_blocks/hive_blocks.go
@@ -11,97 +11,115 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+// hiveBlocks is the unexported implementation of the HiveBlocks interface.
 type hiveBlocks struct {
 	*db.Collection
 }
 
-// creates a new collection in the database
-func New(d *vsc.VscDb) HiveBlocks {
-	return &hiveBlocks{
-		Collection: db.NewCollection(d.DbInstance, "hive_blocks"),
-	}
+// inits and returns a HiveBlocks interface with the underlying hiveBlocks struct.
+func New(vscDb *vsc.VscDb) HiveBlocks {
+	return &hiveBlocks{db.NewCollection(vscDb.DbInstance, "hive_blocks")}
 }
 
-// stores a block in the db
 func (h *hiveBlocks) StoreBlock(block *HiveBlock) error {
 	if block == nil {
 		return fmt.Errorf("block is nil")
 	}
 
-	// insert block into the collection
-	_, err := h.Collection.Collection.InsertOne(context.Background(), block)
+	_, err := h.InsertOne(context.TODO(), bson.M{
+		"type":  "block",
+		"block": block,
+	})
 	if err != nil {
 		return fmt.Errorf("failed to store block: %v", err)
+	}
+
+	// update the last processed block number
+	err = h.StoreLastProcessedBlock(block.BlockNumber)
+	if err != nil {
+		fmt.Printf("Warning: failed to update last processed block number: %v\n", err)
+	}
+	return nil
+}
+
+// deletes all block and metadata documents from the collection.
+func (h *hiveBlocks) ClearBlocks() error {
+	// start a session for tx
+	session, err := h.Database().Client().StartSession()
+	if err != nil {
+		return fmt.Errorf("failed to start session: %v", err)
+	}
+	defer session.EndSession(context.Background())
+
+	// define a tx func
+	callback := func(sessCtx mongo.SessionContext) (interface{}, error) {
+		// delete blocks
+		_, err := h.DeleteMany(sessCtx, bson.M{"type": "block"})
+		if err != nil {
+			return nil, fmt.Errorf("failed to clear blocks collection: %v", err)
+		}
+
+		// delete metadata
+		_, err = h.DeleteMany(sessCtx, bson.M{"type": "metadata"})
+		if err != nil {
+			return nil, fmt.Errorf("failed to clear metadata: %v", err)
+		}
+
+		return nil, nil
+	}
+
+	// execute the tx
+	_, err = session.WithTransaction(context.Background(), callback)
+	if err != nil {
+		return fmt.Errorf("transaction failed: %v", err)
 	}
 
 	return nil
 }
 
-// clears all blocks from this collection
-func (h *hiveBlocks) ClearBlocks() error {
-	_, err := h.Collection.Collection.DeleteMany(context.Background(), bson.M{})
-	return err
-}
-
-// stores the last processed block number in the database
+// upserts the last processed block number in the collection.
 func (h *hiveBlocks) StoreLastProcessedBlock(blockNumber int) error {
-	metaCollection := h.Collection.Collection.Database().Collection("hive_blocks_meta")
-
-	_, err := metaCollection.UpdateOne(context.Background(), bson.M{
-		"type": "last_processed_block",
-	}, bson.M{
-		"$set": bson.M{
-			"block_number": blockNumber,
-		},
-		// upserts so that if no record exists, create it
-	}, options.Update().SetUpsert(true))
+	_, err := h.UpdateOne(context.Background(), bson.M{"type": "metadata"},
+		bson.M{"$set": bson.M{"block_number": blockNumber}},
+		options.Update().SetUpsert(true),
+	)
 	return err
 }
 
-// retrieves the last processed block number from the db
+// retrieves the last processed block number from the collection.
 func (h *hiveBlocks) GetLastProcessedBlock() (int, error) {
-	metaCollection := h.Collection.Collection.Database().Collection("hive_blocks_meta")
-
 	var result struct {
 		BlockNumber int `bson:"block_number"`
 	}
-
-	err := metaCollection.FindOne(context.Background(), bson.M{
-		"type": "last_processed_block",
-	}).Decode(&result)
-
+	err := h.FindOne(context.Background(), bson.M{"type": "metadata"}).Decode(&result)
 	if err != nil {
 		if err == mongo.ErrNoDocuments {
-			return 0, nil // if no doc is found, return 0 as the last processed block
+			fmt.Println("Warning: no last processed block found")
+			return 0, nil
 		}
 		return 0, fmt.Errorf("failed to get last processed block: %v", err)
 	}
-
 	return result.BlockNumber, nil
 }
 
-// fetches blocks within a specified range
-func (h *hiveBlocks) FetchStoredBlocks(startBlock int, endBlock int) ([]HiveBlock, error) {
+// retrieves blocks within a specified range.
+func (h *hiveBlocks) FetchStoredBlocks(startBlock, endBlock int) ([]HiveBlock, error) {
 	filter := bson.M{
-		// creating a range, aka: startBlock <= block_number <= endBlock
-		"block_number": bson.M{
+		"type": "block",
+		"block.block_number": bson.M{
 			"$gte": startBlock,
 			"$lte": endBlock,
 		},
 	}
-
-	// fetch the blocks from the collection
-	cursor, err := h.Collection.Collection.Find(context.Background(), filter)
+	cursor, err := h.Find(context.Background(), filter)
 	if err != nil {
 		return nil, fmt.Errorf("failed to fetch blocks: %v", err)
 	}
 	defer cursor.Close(context.Background())
 
-	// decode the blocks into a slice
 	var blocks []HiveBlock
 	if err := cursor.All(context.Background(), &blocks); err != nil {
 		return nil, fmt.Errorf("failed to decode blocks: %v", err)
 	}
-
 	return blocks, nil
 }
