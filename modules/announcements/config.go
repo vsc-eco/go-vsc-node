503146eeb1d4d90920c0d2a1b43831577b3c1033
Matthew Trent <matthew.rl.trent@gmail.com>
2024-12-03 00:41:58 -0800

    wip: announcement template

--- modules/announcements/config.go
+++ modules/announcements/config.go
@@ -2,8 +2,12 @@ package announcements
 
 import (
 	"crypto/ed25519"
+	"crypto/rand"
+	"fmt"
 	"vsc-node/lib/dids"
 	"vsc-node/modules/config"
+
+	blst "github.com/supranational/blst/bindings/go"
 )
 
 type announcementsConfig struct {
@@ -12,9 +16,25 @@ type announcementsConfig struct {
 }
 
 func NewAnnouncementsConfig() *config.Config[announcementsConfig] {
+	// gen a random seed
+	var seed [32]byte
+	_, err := rand.Read(seed[:])
+	if err != nil {
+		panic(fmt.Errorf("failed to generate random seed: %w", err))
+	}
+
+	// gen blst.SecretKey using the seed
+	blsPrivKey := blst.KeyGen(seed[:])
+
+	// gen Ed25519 key pair using rand reader
+	_, consensusKey, err := ed25519.GenerateKey(rand.Reader)
+	if err != nil {
+		panic(fmt.Errorf("failed to generate Ed25519 key: %w", err))
+	}
+
+	// defaults now for our config if not already provided
 	return config.New(announcementsConfig{
-		// default values are "empty" keys
-		BlsPrivKey:   dids.BlsPrivKey{},
-		ConsensusKey: ed25519.PrivateKey{},
+		BlsPrivKey:   *blsPrivKey,
+		ConsensusKey: consensusKey,
 	})
 }
