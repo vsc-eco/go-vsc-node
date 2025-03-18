2409ac1200c1262f11d5038fda92c17bad4acb57
Geoffrey Casper <gcasper42@gmail.com>
2025-03-18 12:28:41 -0400

    hive: allow WIF to be changed at runtime

--- lib/hive/hive.go
+++ lib/hive/hive.go
@@ -34,7 +34,7 @@ type HiveTransactionCreator interface {
 
 type TransactionBroadcaster struct {
 	Client  *hivego.HiveRpcNode
-	KeyPair *hivego.KeyPair
+	KeyPair func() (*hivego.KeyPair, error)
 }
 
 func (t *TransactionBroadcaster) Broadcast(tx hivego.HiveTransaction) (string, error) {
@@ -145,7 +145,11 @@ func (t *TransactionCrafter) CancelTransferFromSavings(from string, requestId in
 }
 
 func (t *TransactionBroadcaster) Sign(tx hivego.HiveTransaction) (string, error) {
-	return tx.Sign(*t.KeyPair)
+	kp, err := t.KeyPair()
+	if err != nil {
+		return "", err
+	}
+	return tx.Sign(*kp)
 }
 
 func (t *TransactionCrafter) MakeTransaction(ops []hivego.HiveOperation) hivego.HiveTransaction {
