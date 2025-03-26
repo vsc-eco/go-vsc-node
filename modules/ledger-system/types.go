21cdc4c0ff2f8dffd2a8d0baaa5f3caddea8c64f
Vaultec81 <47548474+vaultec81@users.noreply.github.com>
2025-03-26 16:37:26 -0700

    refactor: move ledger types to separate file

---
+++ modules/ledger-system/types.go
@@ -0,0 +1,66 @@
+package ledgerSystem
+
+type LedgerUpdate struct {
+	Id string
+	//Block Index
+	BlockHeight uint64
+	//Block Index: Index of the TX in the block
+	BIdx int64
+	//Op Index: Index of the operation in the TX
+	OpIdx int64
+
+	Owner  string
+	Amount int64
+	Asset  string
+	Memo   string
+	//transfer, withdraw, stake, unstake
+	Type string
+}
+
+type OpLogEvent struct {
+	To     string `json:"to"`
+	From   string `json:"fr"`
+	Amount int64  `json:"am"`
+	Asset  string `json:"as"`
+	Memo   string `json:"mo"`
+	Type   string `json:"ty"`
+
+	//Not parted of compiled state
+	Id          string `json:"id"`
+	BIdx        int64  `json:"-"`
+	OpIdx       int64  `json:"-"`
+	BlockHeight uint64 `json:"-"`
+
+	//Fee for instant stake unstake
+	// Fee int64 `json:"fee,omitempty"`
+	Params map[string]interface{} `json:"params,omitempty"`
+}
+
+type WithdrawParams struct {
+	Id     string `json:"id"`
+	From   string `json:"from"`
+	To     string `json:"to"`
+	Asset  string `json:"asset"`
+	Amount int64  `json:"amount"`
+	Memo   string `json:"memo"`
+
+	BIdx        int64  `json:"bidx"`
+	OpIdx       int64  `json:"opidx"`
+	BlockHeight uint64 `json:"block_height"`
+}
+
+type ConsensusParams struct {
+	Id          string
+	Account     string
+	Amount      int64
+	Type        string
+	BlockHeight uint64
+}
+
+type LedgerSession interface {
+	Done()
+	Revert()
+	AppendOplog(event OpLogEvent)
+	AppendLedger(event LedgerUpdate)
+	GetBalance(account string, blockHeight uint64, asset string) int64
+}
