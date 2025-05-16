48764a7e62b1f8ea706ab037404ef94066e2dfe9
Geoffrey Casper <gcasper42@gmail.com>
2025-05-15 20:31:38 -0400

    db: refactor create index if not exist to collection instance

--- modules/db/collection.go
+++ modules/db/collection.go
@@ -1,10 +1,14 @@
 package db
 
 import (
+	"context"
+	"fmt"
+	"slices"
 	"vsc-node/lib/utils"
 	a "vsc-node/modules/aggregate"
 
 	"github.com/chebyrash/promise"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -41,5 +45,76 @@ func (c *Collection) Start() *promise.Promise[any] {
 
 // Stop implements aggregate.Plugin.
 func (c *Collection) Stop() error {
+	return nil
+}
+
+// CompareIndexOptions compares index options between two IndexModels
+func compareIndexOptions(existingOptions *mongo.IndexSpecification, newOptions *options.IndexOptions) bool {
+	// Compare options like Unique, Sparse, etc.
+	// If any of the options don't match, return false
+	if existingOptions == nil && newOptions == nil {
+		return true
+	}
+
+	if existingOptions == nil || newOptions == nil {
+		return false
+	}
+
+	// Compare relevant fields (add more as needed)
+	if existingOptions.Unique != nil && newOptions.Unique != nil && *existingOptions.Unique != *newOptions.Unique {
+		return false
+	}
+
+	if existingOptions.Sparse != nil && newOptions.Sparse != nil && *existingOptions.Sparse != *newOptions.Sparse {
+		return false
+	}
+
+	if existingOptions.ExpireAfterSeconds != nil && newOptions.ExpireAfterSeconds != nil && *existingOptions.ExpireAfterSeconds != *newOptions.ExpireAfterSeconds {
+		return false
+	}
+
+	// Add more option comparisons as needed
+
+	return true
+}
+
+func (collection *Collection) CreateIndexIfNotExist(indexModel mongo.IndexModel) error {
+	// List all index specifications
+	indexes, err := collection.Indexes().ListSpecifications(context.Background())
+	if err != nil {
+		return fmt.Errorf("failed to list index specifications: %v", err)
+	}
+
+	keys, err := bson.Marshal(indexModel.Keys)
+	if err != nil {
+		return fmt.Errorf("failed to marshal index model: %v", err)
+	}
+
+	// Check if the index already exists and matches the options
+	var indexExists bool
+	for _, existingIndex := range indexes {
+		// Compare the index keys
+		existingKeys, err := bson.Marshal(existingIndex.KeysDocument)
+		if err != nil {
+			return fmt.Errorf("failed to marshal existing index key doc: %v", err)
+		}
+
+		if slices.Equal(existingKeys, keys) {
+			// Compare the options of the index
+			if compareIndexOptions(existingIndex, indexModel.Options) {
+				indexExists = true
+				break
+			}
+		}
+	}
+
+	// If the index doesn't exist, create it
+	if !indexExists {
+		_, err := collection.Indexes().CreateOne(context.Background(), indexModel)
+		if err != nil {
+			return fmt.Errorf("failed to create index: %v", err)
+		}
+	}
+
 	return nil
 }
