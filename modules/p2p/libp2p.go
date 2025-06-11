cdeed7eaa53a19a72d00527e9197de6578b8beb1
vaultec81 <merk@laptop-2>
2025-06-11 10:49:54 -0700

    fix: add more bootstrap nodes

--- modules/p2p/libp2p.go
+++ modules/p2p/libp2p.go
@@ -41,9 +41,16 @@ import (
 
 var BOOTSTRAP = []string{
 	"/dnsaddr/api.vsc.eco/tcp/10720/p2p/12D3KooWFVVQ2xG6ohJ3tQrh3V6zZnRnKPVfQWVH5LjJakzBCs7E",
-	"/ip4/149.56.25.168/tcp/10720/p2p/12D3KooWFVVQ2xG6ohJ3tQrh3V6zZnRnKPVfQWVH5LjJakzBCs7E",  // TODO this is api.vsc.eco, but DNS resolution doesn't work?
-	"/ip4/173.211.12.65/tcp/10720/p2p/12D3KooWGpWrBc5pFx5GHWibczTPrazDCfk8GCETB5Ynb4Dq5L5V",  //@vaultec.vsc
-	"/ip4/147.135.15.155/tcp/10720/p2p/12D3KooWCAE4XrkE4NJL3nqYkXXNhte94rdBDGGVQQJewrDXDVJZ", // mengao
+	"/ip4/149.56.25.168/tcp/10720/p2p/12D3KooWFVVQ2xG6ohJ3tQrh3V6zZnRnKPVfQWVH5LjJakzBCs7E",   // TODO this is api.vsc.eco, but DNS resolution doesn't work?
+	"/ip4/173.211.12.65/tcp/10720/p2p/12D3KooWGpWrBc5pFx5GHWibczTPrazDCfk8GCETB5Ynb4Dq5L5V",   //@vaultec.vsc
+	"/ip4/147.135.15.155/tcp/10720/p2p/12D3KooWCAE4XrkE4NJL3nqYkXXNhte94rdBDGGVQQJewrDXDVJZ",  // mengao
+	"/ip4/188.40.125.182/tcp/10720/p2p/12D3KooWPzZ9RzsCP6BREUFbY1xyZiJ3PPoCW3DFGDhAwExiUazV",  //@spker
+	"/ip4/37.27.190.82/tcp/10720/p2p/12D3KooWDEruPNvPqWc1DN7Fnj6euCZ5QN98YbK9v81wXA9aZqZr",    //@actifit.vsc
+	"/ip4/51.75.151.131/tcp/10720/p2p/12D3KooWQnA1VE8H4QKAkeaEaXd7n1GpLxQXq1MoURn4Lnqhqe3X",   //@arcange.vsc
+	"/ip4/185.130.45.196/tcp/10720/p2p/12D3KooWKUmixKkURVEktxNkn6Keb8LUPk4FdGXM6uAPGsiURw9z",  //@dalz.vsc
+	"/ip4/5.78.133.3/tcp/10720/p2p/12D3KooWA8s64sSSbRLGGXspuU2hkfEkB7tKrcNDBWWjL6V8dC6o",      //@blocktrades.vsc
+	"/ip4/185.130.45.154/tcp/10720/p2p/12D3KooWG4N5gCzbrX2k87baWtcYu2GgRxHvXJ1jmecx5cdxzohH",  //@v4vapp.vsc
+	"/ip4/138.199.230.214/tcp/10720/p2p/12D3KooWHyT797hmbLjgbFjYFpbHrre1EPWzFo876FDhdcbPVG8p", //@tibfox.vsc
 	// "/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN",
 	// "/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa",
 	// "/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb",
