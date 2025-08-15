99ec590ff821b224344c0f46e7ea3a2c201c7921
hal <haln_01@proton.me>
2025-08-15 12:55:59 -0700

    feat: implemented oracle's PubSubServiceParams interface

---
+++ modules/oracle/p2p.go
@@ -0,0 +1,87 @@
+package oracle
+
+import (
+	"context"
+	"encoding/json"
+	"log/slog"
+	"vsc-node/modules/common"
+	libp2p "vsc-node/modules/p2p"
+
+	pubsub "github.com/libp2p/go-libp2p-pubsub"
+	"github.com/libp2p/go-libp2p/core/peer"
+)
+
+const topic = "/vsc/mainet/oracle/v1"
+
+type Msg = *OracleMessage
+
+type Oracle struct {
+	service libp2p.PubSubService[OracleMessage]
+}
+
+type OracleMessage struct{}
+
+type p2pSpec struct {
+	conf   common.IdentityConfig
+	oracle *Oracle
+}
+
+// Topic implements PubSubServiceParams[Msg]
+func (*p2pSpec) Topic() string {
+	return topic
+}
+
+// ValidateMessage implements PubSubServiceParams[Msg]
+func (p *p2pSpec) ValidateMessage(
+	ctx context.Context,
+	from peer.ID,
+	msg *pubsub.Message,
+	parsedMsg Msg,
+) bool {
+	return false
+}
+
+// HandleMessage implements PubSubServiceParams[Msg]
+func (p *p2pSpec) HandleMessage(
+	ctx context.Context,
+	from peer.ID,
+	msg Msg,
+	send libp2p.SendFunc[Msg],
+) error {
+	// TODO: implement this
+	return nil
+}
+
+// HandleRawMessage implements PubSubServiceParams[Msg]
+func (p *p2pSpec) HandleRawMessage(
+	ctx context.Context,
+	rawMsg *pubsub.Message,
+	send libp2p.SendFunc[Msg],
+) error {
+	// Not needed(?)
+	return nil
+}
+
+// ParseMessage implements PubSubServiceParams[Msg]
+func (p *p2pSpec) ParseMessage(data []byte) (Msg, error) {
+	var msg Msg
+	if err := json.Unmarshal(data, msg); err != nil {
+		return nil, err
+	}
+
+	return msg, nil
+}
+
+// SerializeMessage implements PubSubServiceParams[Msg]
+func (p *p2pSpec) SerializeMessage(msg Msg) []byte {
+	jsonBytes, err := json.Marshal(msg)
+	if err != nil {
+		slog.Error(
+			"failed to serialize oracle message.",
+			"msg", msg,
+			"err", err,
+		)
+		return nil
+	}
+	return jsonBytes
+}
