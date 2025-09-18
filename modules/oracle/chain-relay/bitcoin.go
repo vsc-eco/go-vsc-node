13a9f184c6144e67413a6f2cab850be2cf74b220
hal <haln_01@proton.me>
2025-09-17 18:10:34 -0700

    feat: added bitcoin block fetching function

--- modules/oracle/chain-relay/bitcoin.go
+++ modules/oracle/chain-relay/bitcoin.go
@@ -2,10 +2,15 @@ package chainrelay
 
 import (
 	"errors"
+	"fmt"
+	"log"
 	"os"
 	"vsc-node/modules/oracle/p2p"
 
+	"github.com/btcsuite/btcd/btcjson"
+	"github.com/btcsuite/btcd/chaincfg/chainhash"
 	"github.com/btcsuite/btcd/rpcclient"
+	"github.com/btcsuite/btcd/wire"
 )
 
 const bitcoinSymbol = "BTC"
@@ -19,15 +24,6 @@ var (
 
 	errInvalidConf = errors.New("invalid config")
 )
-
-func (b *bitcoinRelayer) Symbol() string {
-	return bitcoinSymbol
-}
-
-// GetBlock implements chainRelay.
-func (b *bitcoinRelayer) GetBlock() (*p2p.BlockRelay, error) {
-	panic("unimplemented")
-}
 
 // Init implements chainRelay.
 func (b *bitcoinRelayer) Init() error {
@@ -49,25 +45,6 @@ func (b *bitcoinRelayer) Init() error {
 		DisableTLS:   true,
 	}
 
-	/*
-		headblock, err := client.GetBlockCount()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		// Get block at specific height
-		blockHash, err := client.GetBlockHash(headblock)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		block, err := client.GetBlock(blockHash)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		log.Printf("Block: %+v", block)
-	*/
 	return nil
 }
 
@@ -76,6 +53,113 @@ func (b *bitcoinRelayer) Shutdown() error {
 	return nil
 }
 
-func (b *bitcoinRelayer) connect() (*rpcclient.Client, error) {
-	return rpcclient.New(&b.rpcConfig, nil)
+// Symbol implements chainRelay.
+func (b *bitcoinRelayer) Symbol() string {
+	return bitcoinSymbol
+}
+
+// GetBlock implements chainRelay.
+// Get the block of depth 6
+func (b *bitcoinRelayer) GetBlock() (*p2p.BlockRelay, error) {
+	btcdClient, err := b.connect(&b.rpcConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	defer btcdClient.Shutdown()
+
+	headblock, err := btcdClient.GetBlockCount()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get headblock: %w", err)
+	}
+
+	blockHeight := headblock - 6 // depth 6
+
+	blockHash, err := btcdClient.GetBlockHash(blockHeight)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// get block header + total fees
+	var (
+		blockChan      = make(chan result[*wire.MsgBlock], 1)
+		blockStatsChan = make(chan result[*btcjson.GetBlockStatsResult], 1)
+	)
+	defer close(blockChan)
+	defer close(blockStatsChan)
+
+	// get block header + average fee
+	go btcdClient.getBlock(blockHash, blockChan)
+	go btcdClient.getBlockStats(blockHash, blockStatsChan)
+
+	var (
+		blockQueryResult      = <-blockChan
+		blockStatsQueryResult = <-blockStatsChan
+	)
+
+	if blockQueryResult.err != nil {
+		return nil, blockQueryResult.err
+	}
+
+	if blockStatsQueryResult.err != nil {
+		return nil, blockQueryResult.err
+	}
+
+	// make block relay
+	var (
+		btcBlock    = blockQueryResult.ok
+		blockHeader = btcBlock.Header
+		avgFee      = blockStatsQueryResult.ok.AverageFee
+	)
+
+	p2pBlockRelay := &p2p.BlockRelay{
+		Hash:       blockHash.String(),
+		Height:     blockHeight,
+		PrevBlock:  blockHeader.PrevBlock.String(),
+		MerkleRoot: blockHeader.MerkleRoot.String(),
+		Timestamp:  blockHeader.Timestamp.UTC(),
+		Fees:       avgFee * int64(len(btcBlock.Transactions)),
+	}
+
+	return p2pBlockRelay, nil
+}
+
+type btcdClient struct {
+	*rpcclient.Client
+}
+
+func (b *bitcoinRelayer) connect(
+	cfg *rpcclient.ConnConfig,
+) (*btcdClient, error) {
+	cx, err := rpcclient.New(cfg, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &btcdClient{cx}, nil
+}
+
+type result[T any] struct {
+	ok  T
+	err error
+}
+
+func (b *btcdClient) getBlock(
+	blockHash *chainhash.Hash,
+	c chan<- result[*wire.MsgBlock],
+) {
+	r := result[*wire.MsgBlock]{nil, nil}
+	r.ok, r.err = b.GetBlock(blockHash)
+	c <- r
+}
+
+func (b *btcdClient) getBlockStats(
+	blockHash *chainhash.Hash,
+	c chan<- result[*btcjson.GetBlockStatsResult],
+) {
+	var (
+		blockStatsFilter = []string{"avgfee"}
+		r                = result[*btcjson.GetBlockStatsResult]{nil, nil}
+	)
+	r.ok, r.err = b.GetBlockStats(blockHash, &blockStatsFilter)
+	c <- r
 }
