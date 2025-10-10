0cde1e7195e3758828dcddd1eccf790302ee5db5
Milo Ridenour <milo.hpr@gmail.com>
2025-10-10 22:34:01 +0900

    added main map/unmap handlers, persisted data to disc

--- cmd/mapping-bot/mapper/init.go
+++ cmd/mapping-bot/mapper/init.go
@@ -1,21 +1,48 @@
 package mapper
 
-import "sync"
+import (
+	"context"
+	"strconv"
+	"sync"
+
+	"github.com/ipfs/go-datastore"
+	flatfs "github.com/ipfs/go-ds-flatfs"
+)
+
+var lastBlockKey = datastore.NewKey("lastblock")
+var observedTxsKey = datastore.NewKey("observed")
+var sentTxsKey = datastore.NewKey("senttxs")
 
 type MapperState struct {
-	Mutex           sync.Mutex
-	LastBlockHeight uint32
-	ObservedTxs     map[string]bool
-	TxSpends        map[string]*SigningData
-	LimboTxs        map[string]bool // set for fast lookup
+	Mutex                sync.Mutex
+	FfsDatastore         *flatfs.Datastore
+	LastBlockHeight      uint32
+	ObservedTxs          map[string]bool
+	AwaitingSignatureTxs *AwaitingSignature
+	// txs that have been posted, but haven't been seen in a block yet
+	SentTxs map[string]bool
 }
 
-func NewMapperState() *MapperState {
-	return &MapperState{
-		Mutex:           sync.Mutex{},
-		LastBlockHeight: 918397,
-		ObservedTxs:     make(map[string]bool),
-		TxSpends:        make(map[string]*SigningData),
-		LimboTxs:        make(map[string]bool),
+func NewMapperState(ffs *flatfs.Datastore) (*MapperState, error) {
+	unsignedTxs := &AwaitingSignature{
+		Txs:    make(map[string]*SignedData),
+		Hashes: make(map[string]*HashMetadata),
 	}
+	ctx := context.TODO()
+	heightVal, err := ffs.Get(ctx, lastBlockKey)
+	if err != nil {
+		return nil, err
+	}
+	heightInt, err := strconv.Atoi(string(heightVal))
+	if err != nil {
+		return nil, err
+	}
+	return &MapperState{
+		Mutex:                sync.Mutex{},
+		FfsDatastore:         ffs,
+		LastBlockHeight:      uint32(heightInt),
+		ObservedTxs:          make(map[string]bool),
+		SentTxs:              make(map[string]bool),
+		AwaitingSignatureTxs: unsignedTxs,
+	}, nil
 }
