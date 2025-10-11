e369d83b1a43049e71d814567f0cf8f2217ac29b
hal <haln_01@proton.me>
2025-10-11 11:41:31 +0900

    post endpoint + datastore interface

---
+++ cmd/mapping-bot/addresses.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+
+	"github.com/btcsuite/btcd/btcutil"
+	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/btcsuite/btcd/txscript"
+)
+
+var (
+	network   = &chaincfg.TestNet3Params
+	pubKeyHex = ""
+)
+
+func makeBtcAddress(vscAddr string) (string, error) {
+	// TODO: add validation to vsc addr
+
+	pubKeyBytes, err := hex.DecodeString(pubKeyHex)
+	if err != nil {
+		return "", err
+	}
+
+	tag := sha256.Sum256([]byte(vscAddr))
+
+	scriptBuilder := txscript.NewScriptBuilder()
+	scriptBuilder.AddData(pubKeyBytes)              // Push pubkey
+	scriptBuilder.AddOp(txscript.OP_CHECKSIGVERIFY) // OP_CHECKSIGVERIFY
+	scriptBuilder.AddData(tag[:])                   // Push tag/bits
+
+	script, err := scriptBuilder.Script()
+	if err != nil {
+		return "", err
+	}
+
+	witnessProgram := sha256.Sum256(script)
+	addressWitnessScriptHash, err := btcutil.NewAddressWitnessScriptHash(
+		witnessProgram[:],
+		network,
+	)
+	if err != nil {
+		return "", err
+	}
+	address := addressWitnessScriptHash.EncodeAddress()
+
+	return address, nil
+}
---
+++ cmd/mapping-bot/database/datastore.go
@@ -0,0 +1,96 @@
+package database
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"os"
+	"sync"
+
+	"github.com/ipfs/go-datastore"
+	flatfs "github.com/ipfs/go-ds-flatfs"
+)
+
+var (
+	ErrAddrExists   = errors.New("key exists in datastore")
+	ErrAddrNotFound = errors.New("address not found")
+)
+
+type MappingBotDatabase struct {
+	db  *flatfs.Datastore
+	mtx *sync.Mutex
+}
+
+func New(path string) (*MappingBotDatabase, error) {
+	if err := os.MkdirAll(path, 0755); err != nil {
+		return nil, err
+	}
+
+	// uses default sharding
+	fs, err := flatfs.CreateOrOpen(path, flatfs.NextToLast(2), false)
+	if err != nil {
+		return nil, err
+	}
+
+	mdb := &MappingBotDatabase{
+		db:  fs,
+		mtx: &sync.Mutex{},
+	}
+
+	return mdb, nil
+}
+
+// store key in datastore, returns any operational error, or ErrAddrExists if
+// key exists in datastore.
+func (m *MappingBotDatabase) InsertAddressMap(
+	ctx context.Context,
+	btcAddr, vscAddr string,
+) error {
+	key := datastore.NewKey(btcAddr)
+
+	m.mtx.Lock()
+	defer m.mtx.Unlock()
+
+	// checks for address existence
+	exists, err := m.db.Has(ctx, key)
+	if err != nil {
+		return fmt.Errorf("failed to query key [%s]: %w", btcAddr, err)
+	}
+
+	if exists {
+		return ErrAddrExists
+	}
+
+	// value encoding
+	if err := m.db.Put(ctx, key, []byte(vscAddr)); err != nil {
+		return fmt.Errorf("failed to put key [%s]: %w", btcAddr, err)
+	}
+
+	return nil
+}
+
+// return vsc address from btcAddr, if vsc address not found, returns
+// ErrAddrNotFound, otherwise any operational error
+func (m *MappingBotDatabase) GetVscAddress(
+	ctx context.Context,
+	btcAddr string,
+) (string, error) {
+	key := datastore.NewKey(btcAddr)
+
+	m.mtx.Lock()
+	defer m.mtx.Unlock()
+
+	vscAddr, err := m.db.Get(ctx, key)
+	if err != nil {
+		if errors.Is(err, datastore.ErrNotFound) {
+			return "", ErrAddrNotFound
+		}
+
+		return "", fmt.Errorf(
+			"failed to get vscAddress [btcAddr: %s]: %w",
+			btcAddr, err,
+		)
+	}
+
+	return string(vscAddr), nil
+}
