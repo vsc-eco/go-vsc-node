df12f23bb9c6f452fd09e109980442ae284be3b5
techcoderx <git@techcoderx.com>
2026-01-24 17:03:21 +0800

    config: testnet starts from block 2

--- modules/common/system-config/system-config.go
+++ modules/common/system-config/system-config.go
@@ -86,7 +86,7 @@ func TestnetConfig() SystemConfig {
 		netId:          "vsc-testnet",
 		hiveChainId:    "18dcf0a285365fc58b71f18b3d3fec954aa0c141c44e4e5cb4cf777b9eab274e",
 		gatewayWallet:  "vsc.testnet",
-		startHeight:    1,
+		startHeight:    2,
 		consensusParams: params.ConsensusParams{
 			MinStake:       params.CONSENSUS_MINIMUM,
 			MinRcLimit:     params.MINIMUM_RC_LIMIT,
