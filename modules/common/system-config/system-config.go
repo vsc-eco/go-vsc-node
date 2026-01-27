9f05f77395e0f13e3fcb29fd66530fd08020aba8
techcoderx <git@techcoderx.com>
2026-01-28 01:01:01 +0800

    config: fix testnet gateway name

--- modules/common/system-config/system-config.go
+++ modules/common/system-config/system-config.go
@@ -85,7 +85,7 @@ func TestnetConfig() SystemConfig {
 		network:        "testnet",
 		netId:          "vsc-testnet",
 		hiveChainId:    "18dcf0a285365fc58b71f18b3d3fec954aa0c141c44e4e5cb4cf777b9eab274e",
-		gatewayWallet:  "vsc.testnet",
+		gatewayWallet:  "vsc.gateway",
 		startHeight:    2,
 		consensusParams: params.ConsensusParams{
 			MinStake:       params.CONSENSUS_MINIMUM,
